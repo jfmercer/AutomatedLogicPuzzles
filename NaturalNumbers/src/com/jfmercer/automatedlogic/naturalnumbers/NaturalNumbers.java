7d898c4cf1ad77f2014cf7b90ed4b3785f00cfb4
John F. Mercer <john.f.mercer@gmail.com>
2014-11-22 14:09:36 -0500

    minor tweaks to JavaDoc comments

--- NaturalNumbers/src/com/jfmercer/automatedlogic/naturalnumbers/NaturalNumbers.java
+++ NaturalNumbers/src/com/jfmercer/automatedlogic/naturalnumbers/NaturalNumbers.java
@@ -9,10 +9,10 @@ package com.jfmercer.automatedlogic.naturalnumbers;
 public class NaturalNumbers
 {
    /**
-    * Sums all natural numbers less
+    * Sums all natural numbers less than x
+    * which are divisible by 3 or 5.
     *
     * @param x
-    * @return
     */
    public void calculateNumberTotal(int x) {
       int total = 0;
@@ -30,6 +30,13 @@ public class NaturalNumbers
       printResults(x, total);
    }
 
+   /**
+    * Prints out the results of method
+    * calculateNumberTotal;
+    *
+    * @param x
+    * @param total
+    */
    private void printResults(int x, int total) {
       System.out.print("You entered " + x + " and your total is " + total + ".\n");
    }
