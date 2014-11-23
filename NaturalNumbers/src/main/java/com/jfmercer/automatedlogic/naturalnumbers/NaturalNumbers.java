b10e239d8086b8473499e38581ac594955d62faa
John F. Mercer <john.f.mercer@gmail.com>
2014-11-22 21:22:07 -0500

    deletes old driver & printouts

--- NaturalNumbers/src/main/java/com/jfmercer/automatedlogic/naturalnumbers/NaturalNumbers.java
+++ NaturalNumbers/src/main/java/com/jfmercer/automatedlogic/naturalnumbers/NaturalNumbers.java
@@ -6,7 +6,7 @@ package com.jfmercer.automatedlogic.naturalnumbers;
  * @author: John Mercer
  * @version: AutomatedLogicPuzzles
  */
-public class NaturalNumbers
+public final class NaturalNumbers
 {
    /**
     * Sums all natural numbers less than x
@@ -18,9 +18,7 @@ public class NaturalNumbers
       int total = 0;
 
       if(x < 0) {
-         System.out.print("You must enter a natural number. " + x + " will be converted to a " +
-                                  "positive number.\n");
-         x = x * -1;
+         x *= -1;
       }
 
       for(int i = 0; i < x; i++) {
@@ -29,19 +27,7 @@ public class NaturalNumbers
          }
       }
 
-      printResults(x, total);
-
       return total;
    }
 
-   /**
-    * Prints out the results of method
-    * calculateNumberTotal;
-    *
-    * @param x The original number entered.
-    * @param total The total from the calculateNumberTotal method.
-    */
-   private static void printResults(int x, int total) {
-      System.out.print("You entered " + x + " and your total is " + total + ".\n");
-   }
 }
